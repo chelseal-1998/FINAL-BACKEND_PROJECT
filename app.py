1819457cde2263e1a506d5320b820041f5136819
Chelsea <chelslaguma@gmail.com>
2021-02-24 16:20:36 +0200

    new

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def init_sqlite_db():
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
@@ -54,6 +61,7 @@ def show_data():
     data = []
     try:
         with sqlite3.connect('database.db') as con:
+            con.row_factory = dict_factory
             cur = con.cursor()
             cur.execute("SELECT * FROM blogs")
             data = cur.fetchall()
@@ -62,8 +70,23 @@ def show_data():
         print("There was an error fetching results from the database.")
     finally:
         con.close()
+    return jsonify(data)
 
-    # return render_template('records.html', data=data)
+
+@app.route('/show-data/<int:data_id>/', methods=['GET'])
+def show_data(data_id):
+    data = []
+    try:
+        with sqlite3.connect('database.db') as con:
+            con.row_factory = dict_factory
+            cur = con.cursor()
+            cur.execute("SELECT * FROM blogs WHERE id=" + str(data_id))
+            data = cur.fetchall()
+    except Exception as e:
+        con.rollback()
+        print("There was an error fetching results from the database.")
+    finally:
+        con.close()
     return jsonify(data)
 
 
